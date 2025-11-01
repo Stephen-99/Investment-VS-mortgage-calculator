1fd81e1632edc847a87c22b1e7c0fbfc9f4b39e8
Stephen-99 <56374726+Stephen-99@users.noreply.github.com>
2025-11-01 21:14:27 +0800

    slight update

--- expenses.py
+++ expenses.py
@@ -7,21 +7,21 @@ GAS = 220       #Every 3 months
 RATES = 3200    #Yearly 
 FOOD = 400      #Weekly
 
-OTHER_EXPENSES = 280   #Fortnightly
+OTHER_EXPENSES = 375 + 490  #Fortnightly (expenses + donations)
 
 #TBC
 FORTNIGHTLY_INCOME = 3318
 
 
 def CalcForntighlyExpensesEstimate():
-    water = WATER/3 * 6/26
+    water = WATER * 4/5 * 6/26
     elec = ELEC/3 * 6/26
     gas = GAS/3 * 4/26
     food = FOOD/3 * 2
     rates = RATES * 2/3 /26
     other = OTHER_EXPENSES
     expenses = water + elec + gas + food + rates + other
-    print(f"Fortnightly expenses\nWater: {water:.2f}\nPower: {elec:.2f}\nGas: {gas:.2f}\nFood: {food:.2f}\nRates: {rates:.2f}\nOther: {other:.2f}\nTotal: {expenses:.2f}")
+    print(f"Fortnightly expenses\nWater: {water:.2f}\nPower: {elec:.2f}\nGas: {gas:.2f}\nFood: {food:.2f}\nRates: {rates:.2f}\nOther: {other:.2f}\nHouse related total: {expenses-other:.2f}\nTotal: {expenses:.2f}")
 
     return expenses
 
